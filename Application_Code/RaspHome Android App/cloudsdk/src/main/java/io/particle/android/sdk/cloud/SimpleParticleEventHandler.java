9be23aad69026a7241f8c243fbaca7449dbc65df
vraje <vasundhara597@gmail.com>
2018-07-23 18:04:28 +0000

    android app updated

--- Application_Code/RaspHome Android App/cloudsdk/src/main/java/io/particle/android/sdk/cloud/SimpleParticleEventHandler.java
+++
@@ -1,9 +0,0 @@
-package io.particle.android.sdk.cloud;
-
-/**
- * Created by Julius.
- */
-
-public interface SimpleParticleEventHandler {
-    void onEvent(String eventName, ParticleEvent particleEvent);
-}
